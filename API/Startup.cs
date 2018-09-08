1449294b5dbd9a35fd5a894f74d133317899df99
Michael Jimma <michaeljimma@gmail.com>
2018-09-08 00:19:10 -0400

    After undo error interceptor

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,11 +4,14 @@ using AutoMapper;
 using DAL.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using System.Net;
 using System.Text;
 
 namespace API
@@ -70,7 +73,23 @@ namespace API
       {
         app.UseDeveloperExceptionPage();
       }
+      else
+      {
+        app.UseExceptionHandler(builder =>
+        {
+          builder.Run(async context =>
+          {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
+            var error = context.Features.Get<IExceptionHandlerFeature>();
+            if (error != null)
+            {
+              context.Response.AddApplicationError(error.Error.Message);
+              await context.Response.WriteAsync(error.Error.Message);
+            }
+          });
+        });
+      }
       app.UseCors(q => q.AllowAnyOrigin()
       .AllowAnyMethod().AllowAnyHeader());
 
